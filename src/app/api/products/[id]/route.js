e659d2bdb178f50140469e93b24238288c0e89e0
Foxy20122012 <malvarezm26@miumg.edu.gt>
2023-11-27 00:43:39 -0600

    Rest api terminada y manejador de errores

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,14 +1,98 @@
 import { NextResponse } from "next/server";
+import { conn } from '../../../../libs/mysql'
 
-export function GET(){
-    return NextResponse.json({ message: "obteniendo un producto" })
+
+export async function GET(request, {params}){
+    try {
+        const result =await conn.query('SELECT * FROM product WHERE id = ?', [params.id])
+        if(result.length === 0){
+            return NextResponse.json(
+                {
+                message: "Product not found",
+            },
+            {
+                status: 404
+            }
+            )
+        }
+        console.log(result)
+        return NextResponse.json(result[0])
+    } catch (error) {
+        return NextResponse.json(
+            {
+                message: error.message
+            },
+            {
+                status: 500
+            }
+        )
+    }
 }
 
-export function DELETE(){
-    return NextResponse.json({ message: "eliminando un producto" })
-}
+export async function DELETE(request, { params }) {
+    try {
+      const result = await conn.query("DELETE FROM product WHERE id = ?", [
+        params.id,
+      ]);
+  
+      if (result.affectedRows === 0) {
+        return NextResponse.json(
+          {
+            message: "Producto no encontrado",
+          },
+          {
+            status: 404,
+          }
+        );
+      }
+  
+      return new Response(null, {
+        status: 204,
+      });
+    } catch (error) {
+      return NextResponse.json(
+        {
+          message: error.message,
+        },
+        { status: 500 }
+      );
+    }
+  }
 
-export function PUT(){
-    return NextResponse.json({ message: "actualizando un producto" })
+export async function PUT(request, { params }) {
+    try {
+        
+    const data = await request.json()
+
+    const result = await conn.query('UPDATE product SET ? WHERE id = ?', [data, params.id])
+    if(result.affectedRows === 0) {
+        return NextResponse.json(
+            {
+                message: "Product not found"
+            },
+            {
+                status: 404
+            }
+        )
+    }
+    
+    const updateProduct = await conn.query('SELECT * FROM product WHERE id = ?',
+     [params.id]);
+
+
+    console.log(result)
+    return NextResponse.json(
+        updateProduct[0]
+       )
+    } catch (error) {
+        return NextResponse.json(
+            {
+                message: error.message
+            },
+            {
+                status: 500
+            }
+        )
+    }
 }
 
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,10 +1,22 @@
 import { NextResponse } from "next/server";
 import { conn } from '../../../libs/mysql'
 
-export function GET() {
-  return NextResponse.json({
-    message: "Listando Productos"
-  });
+export async function GET() {
+  try {
+    const results = await conn.query('SELECT * FROM product')
+  return NextResponse.json(
+   results
+  );
+  } catch (error) {
+    return NextResponse.json(
+      {
+      message: error.message
+    },
+    {
+      status: 500
+    }
+    )
+  }
 }
 
 export async function POST (request) {
